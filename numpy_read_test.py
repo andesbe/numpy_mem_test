ab8122431cb7f64eca608f597d852263b42a063b
andesbe <bendiksenanders@gmail.com>
2022-11-22 23:17:40 +0100

    cleaned up a little

--- numpy_read_test.py
+++ numpy_read_test.py
@@ -1,15 +1,14 @@
-#create a function that reads in all the parquet files in a directory, declare a numpy array with the same shape as the data, and then fill the array with the data and returns the array
 
 def read_parquet_files(path):
-    #get the list of all the files in the directory
+    """
+    Read parquet files from a given path and declares a numpy array of the relevant size in order to avoid
+    memory allocation during the loop.
+    :param path: path to the parquet files
+    :return: numpy array"""
     files = os.listdir(path)
-    #get the number of files in the directory
     num_files = len(files)
-    #get the shape of the data
     data_shape= pd.read_parquet(path + files[0]).shape
-    #create a numpy array with the same shape as the data
     data = np.zeros(data_shape)
-    #loop through the files and fill the numpy array with the data
     for i in range(num_files):
         data[i] = pd.read_parquet(path + files[i])
     return data
